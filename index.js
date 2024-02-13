285971887bed2903f9b2582ad24ea6a4c144f7b0
Daniel <33257@ma-web.nl>
2024-02-13 16:42:10 +0100

    auto massege ai chatbot

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv/config');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { default: OpenAI } = require('openai');
 
+const fs = require('fs');
+
+const jsonData = fs.readFileSync('data.json', 'utf-8');
+const messageObject = JSON.parse(jsonData);
 
 
 const client = new Client({
@@ -19,6 +23,29 @@ client.on('ready', () => {
 const openai = new OpenAI({
     apiKey: process.env.API_KEY,
 });
+
+let currentMessageIndex = 0;
+
+const interval = 10 * 1000; // 24 hours
+setInterval(() => {
+    const currentMessageObj = messageObject.messages[currentMessageIndex];
+    if (currentMessageObj) {
+        const currentMessageKey = Object.keys(currentMessageObj)[0];
+        const currentMessage = currentMessageObj[currentMessageKey];
+
+        const channelId = process.env.CHANNEL_ID;
+        const channel = client.channels.cache.get(channelId);
+
+        if (channel) {
+            channel.send(currentMessage);
+        }
+        
+        currentMessageIndex = (currentMessageIndex + 1) % messageObject.messages.length;
+    } else {
+        console.error('Error: Messages array is empty or undefined.');
+    }
+}, interval);
+
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
